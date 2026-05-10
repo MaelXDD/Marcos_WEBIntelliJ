e243b707cb714d0a2a46c2eea46c55c92605958f
svamnpent <stefanovaldeos2003@gmail.com>
2026-05-09 20:33:10 -0500

    bloqueo spring security quitado

--- src/main/java/utp/phantom/phantom/config/SecurityConfig.java
+++ src/main/java/utp/phantom/phantom/config/SecurityConfig.java
@@ -2,26 +2,33 @@ package utp.phantom.phantom.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
-@EnableWebSecurity
 public class SecurityConfig {
 
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+
         http
-                // Permitir todas las rutas sin login
-                .authorizeHttpRequests(auth -> auth
-                        .anyRequest().permitAll()
-                )
-                // Deshabilitar CSRF para que el fetch AJAX funcione
                 .csrf(csrf -> csrf.disable())
-                // Deshabilitar el login automático de Spring Security
-                .formLogin(form -> form.disable())
-                .httpBasic(basic -> basic.disable());
+
+                .authorizeHttpRequests(auth -> auth
+
+
+                        .requestMatchers(
+                                "/carrito/**",
+                                "/css/**",
+                                "/js/**",
+                                "/images/**"
+                        ).permitAll()
+
+                        .anyRequest().authenticated()
+                )
+
+                .formLogin(Customizer.withDefaults());
 
         return http.build();
     }
