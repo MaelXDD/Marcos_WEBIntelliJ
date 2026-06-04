12dd1fa76bcc617f6f1066894b058ee8e286b4ba
rosa2323-svg <rosagraci2323@gmail.com>
2026-06-03 22:18:46 -0500

    Actualización en el repositorio (ProductoRepository)

--- src/main/java/utp/phantom/phantom/repository/ProductoRepository.java
+++ src/main/java/utp/phantom/phantom/repository/ProductoRepository.java
@@ -1,6 +1,8 @@
 package utp.phantom.phantom.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import utp.phantom.phantom.model.Producto;
 import java.util.List;
 
@@ -9,4 +11,9 @@ public interface ProductoRepository extends JpaRepository<Producto, Long> {
     List<Producto> findByCategoriaId(Long categoriaId);
 
     List<Producto> findByNombreContainingIgnoreCaseOrDescripcionContainingIgnoreCase(String nombre, String descripcion);
+
+    @Query("SELECT p FROM Producto p WHERE LOWER(p.nombre) LIKE LOWER(CONCAT('%', :keyword, '%'))")
+    List<Producto> buscarPorNombre(@Param("keyword") String keyword);
+
+    List<Producto> findByStockLessThanEqual(Integer limite);
 }
