4ee79ae0035286edcf3c541aa95c514fa7fd60c5
Sebas236-em <andres.sebastian.espinal@gmail.com>
2026-06-04 21:30:34 -0500

    ADMIN implementado

--- src/main/java/utp/phantom/phantom/config/SecurityConfig.java
+++ src/main/java/utp/phantom/phantom/config/SecurityConfig.java
@@ -1,12 +1,16 @@
 package utp.phantom.phantom.config;
 
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.core.Authentication;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.AuthenticationSuccessHandler;
 import utp.phantom.phantom.service.CustomUserDetailsService;
 
 @Configuration
@@ -21,6 +25,23 @@ public class SecurityConfig {
     }
 
     @Bean
+    public AuthenticationSuccessHandler customSuccessHandler() {
+        return (HttpServletRequest request,
+                HttpServletResponse response,
+                Authentication authentication) -> {
+
+            boolean esAdmin = authentication.getAuthorities().stream()
+                    .anyMatch(a -> a.getAuthority().equals("ROLE_ADMIN"));
+
+            if (esAdmin) {
+                response.sendRedirect("/admin/productos");
+            } else {
+                response.sendRedirect("/");
+            }
+        };
+    }
+
+    @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
         http
                 .csrf(csrf -> csrf.disable())
@@ -32,14 +53,14 @@ public class SecurityConfig {
                                 "/CSS/**", "/Imagenes/**", "/JS/**",
                                 "/imagenes/**", "/categoria/**",
                                 "/carrito/**",
-                                "/pago/**",
-                                "/admin/**"
+                                "/pago/**"
                         ).permitAll()
+                        .requestMatchers("/admin/**").hasRole("ADMIN")
                         .anyRequest().authenticated()
                 )
                 .formLogin(form -> form
                         .loginPage("/login")
-                        .defaultSuccessUrl("/", true)
+                        .successHandler(customSuccessHandler())
                         .failureUrl("/login?error=true")
                         .permitAll()
                 )
