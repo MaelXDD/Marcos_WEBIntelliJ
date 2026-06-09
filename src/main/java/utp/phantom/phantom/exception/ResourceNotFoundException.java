70ec2a52c88c76028519f6ec0023c185e44a68b7
MaelXD <fiestasjheins@gmail.com>
2026-06-09 00:39:38 -0500

    Exception Handler

---
+++ src/main/java/utp/phantom/phantom/exception/ResourceNotFoundException.java
@@ -0,0 +1,7 @@
+package utp.phantom.phantom.exception;
+
+public class ResourceNotFoundException extends RuntimeException {
+    public ResourceNotFoundException(String message) {
+        super(message);
+    }
+}
